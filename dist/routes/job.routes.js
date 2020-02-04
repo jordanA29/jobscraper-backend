e611064771b827bf56f76dd61dc8c3760b358b85
Jordan Arzel <jordan.arzel@protostellar.eu>
2020-02-04 17:00:42 +0100

    rm cached for gitignore

--- dist/routes/job.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const job_controller_1 = require("../controllers/job.controller");
-const router = express_1.Router();
-router.get('/', job_controller_1.default);
-exports.default = router;
-//# sourceMappingURL=job.routes.js.map
